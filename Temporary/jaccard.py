34082a9c577cb80db01ab9d4e22847faee56e947
nik7273 <nik8470@gmail.com>
2015-02-28 22:44:03 -0800

    fixing files

--- Temporary/jaccard.py
+++ Temporary/jaccard.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
+import nltk
+from nltk.collocations import *
+def bigramFinder(FileA):
+    bigram_measures = nltk.collocations.BigramAssocMeasures()
+    bigString = ""
+    with open(FileA,'rb') as infile:
+        for line in infile:
+            nltk.word_tokenize(line)
+            bigString += (line + " ")
+    finder = BigramCollocationFinder.from_words(bigString)  
+    bigrams = finder.nbest(bigram_measures.pmi, 20)
+    print bigrams
 
-jaccardset1 = []
-with open('../Temporary/raw-text-files/sanitizted','rb') as infile1:
-    for word in infile1.read():
-        jaccardset1 += word
-jaccardset2 = []
-with open('../Temporary/raw-text-files/sanitized2','rb') as infile1:
-    for word in infile1.read():
-        jaccardset2 += word
-jaccardset3 = []
-with open('../Temporary/raw-text-files/sanitized3','rb') as infile1:
-    for word in infile1.read():
-        jaccardset3 += word
+bigramFinder("sanitizted")
+bigramFinder("sanitized2")
+bigramFinder("sanitized3")
 
+def jaccard(file1, file2):
+    jaccardset1 = []
+    with open(file1,'rb') as infile:
+        for word in infile.read().splitlines():
+            jaccardset1 += word
+    jaccardset2 = []
+    with open(file2, 'rb') as infile:
+        for word in infile.read().splitlines():
+            jaccardset2 += word
+    print float(len(set(jaccardset1)&set(jaccardset2))/len(set(jaccardset1)|set(jaccardset2)))
+    
+jaccard('sanitizted', 'sanitized2')
+jaccard('sanitizted', 'sanitized3')
+jaccard('sanitized2', 'sanitized3')
 
-
-jaccard = float(len(set(jaccardset1)&set(jaccardset2))/len(set(jaccardset1)|set(jaccardset2)))
-jaccard2 = float(len(set(jaccardset2)&set(jaccardset3))/len(set(jaccardset2)|set(jaccardset3)))
-jaccard3 = float(len(set(jaccardset1)&set(jaccardset3))/len(set(jaccardset1)|set(jaccardset3)))
-
-print jaccard
-print jaccard2
-print jaccard3
