69cd867675f0fc1181ebad36f4dc0f9c6c5ed97e
Michael Chary <mac389@gmail.com>
2015-04-04 09:56:49 -0400

    creates new filename if text file already exists

--- Temporary/findRelevantArticles.py
+++ Temporary/findRelevantArticles.py
@@ -1,31 +1,32 @@
 # -*- coding: utf-8 -*-
 #Search Wikipedia for Heart Attack
-import wikipedia, codecs, itertools
+import wikipedia, codecs, itertools, os, time
 from awesome_print import ap 
 from pprint import pprint
 
 relevant_categories = {'medical','emergencies','disease'}
 
-
 def findRelevantArticles(term):
-    articles,suggestion = wikipedia.search(term, suggestion = True)
-    relList = []
-    with codecs.open("HeartText.txt", 'wb', 'utf-8') as outfile:
+    articles = wikipedia.search(term) #Setting suggestion = False (default value); No clear use for it now
+    filename = '%s.txt'%term
+    if os.path.isfile(filename):
+        filename += time.strftime("%Y%m%d-%H%M%S")
+
+    with codecs.open(filename, 'a+', 'utf-8') as outfile:
         for article in articles:
             try: 
                 article = wikipedia.page(article)
                 category_keywords = set(list(itertools.chain.from_iterable([category.lower().split() for category in article.categories])))
-                if category_keywords & relevant_categories:
+                if len(category_keywords & relevant_categories) > 0:
                     print>>outfile,article.content
-                    relList += article
             except wikipedia.exceptions.PageError as e:
-                pprint(e)
+                #pprint(e)
+                pass #Why print errors I do nothing with?
             except wikipedia.exceptions.DisambiguationError as e:
                 for article in e.options:
                     article = wikipedia.page(article)
-                    if any([relevant_category in article.categories for relevant_category in relevant_categories]):
+                    category_keywords = set(list(itertools.chain.from_iterable([category.lower().split() for category in article.categories])))
+                    if len(category_keywords & relevant_categories) > 0:
                         print>>outfile,article.content
-                        relList += article
-    return relList
 
 findRelevantArticles("Heart Attack")
