7412eff202c50e1dfbf9e9ff67f926b57346e8fa
Michael Chary <mac389@gmail.com>
2015-04-04 09:19:09 -0400

    update

--- Temporary/wikipediaParse.py
+++ Temporary/wikipediaParse.py
@@ -20,18 +20,3 @@ with codecs.open("HeartText.txt", 'wb', 'utf-8') as outfile:
                 article = wikipedia.page(article)
                 if any([relevant_category in article.categories for relevant_category in relevant_categories]):
                     print>>outfile,article.content
-
-'''        
-relevant_articles = []
-#List of relevant articles pertaining to the subject
-#Adds relevant articles to list based on their matching ontology (categories)
-relevant = []
-for article in objectList:
-    categories = article.categories
-    for category in categories:
-        for stdcategory in wikipedia.page(title = "Heart", auto_suggest = True, redirect = True, preload = False).categories:
-            if category == stdcategory:
-                if article not in relevant:
-                    relevant.append(article)
-'''
-
