d1ff574cbdf4635eb0df984097bd76c494d30d6d
nik7273 <nik8470@gmail.com>
2015-04-03 22:50:16 -0700

    INCOMPLETED
    -need to finish finding jaccards
    -improve by building on Findrelevant Articles
    -turned find relevant articles .py into a function to be imported

--- Temporary/findRelevantArticles.py
+++ Temporary/findRelevantArticles.py
@@ -4,23 +4,27 @@ import wikipedia, codecs
 from awesome_print import ap 
 from pprint import pprint
 
-articles,suggestion = wikipedia.search("Heart Attack", results = 10, suggestion = True)
 
-relevant_categories = ['medical','emergencies','disease']
-with codecs.open("HeartText.txt", 'wb', 'utf-8') as outfile:
-    for article in articles:
-        try: 
-            article = wikipedia.page(article)
-            if any([relevant_category in article.categories for relevant_category in relevant_categories]):
-                print>>outfile,article.content
-        except wikipedia.exceptions.PageError as e:
-            pprint(e)
-        except wikipedia.exceptions.DisambiguationError as e:
-            for article in e.options:
+def findRelevantArticles():
+    articles,suggestion = wikipedia.search("Heart Attack", results = 10, suggestion = True)
+    relList = []
+    relevant_categories = ['medical','emergencies','disease']
+    with codecs.open("HeartText.txt", 'wb', 'utf-8') as outfile:
+        for article in articles:
+            try: 
                 article = wikipedia.page(article)
                 if any([relevant_category in article.categories for relevant_category in relevant_categories]):
                     print>>outfile,article.content
-
+                    relList += article
+            except wikipedia.exceptions.PageError as e:
+                pprint(e)
+            except wikipedia.exceptions.DisambiguationError as e:
+                for article in e.options:
+                    article = wikipedia.page(article)
+                    if any([relevant_category in article.categories for relevant_category in relevant_categories]):
+                        print>>outfile,article.content
+                        relList += article
+    return relList
 '''        
 relevant_articles = []
 #List of relevant articles pertaining to the subject
